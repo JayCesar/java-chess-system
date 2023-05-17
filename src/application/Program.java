c1a60fe127754e71e86a298792381e23b9d7f791
JayCesar <batistajulio215@gmail.com>
2023-05-16 23:26:30 -0300

    update: add Special move - promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,17 +10,15 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
-
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(!chessMatch.getCheckMate()) {	
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -40,18 +38,23 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
-			catch(ChessException e) {
-				System.out.print(e.getMessage());
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch(InputMismatchException e) {
-				System.out.print(e.getMessage());
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
 	}
-
 }
